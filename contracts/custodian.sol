9191f0337b41f88fb0abafd77fc799280d8fc1c1
Anis B <anis@Aniss-MacBook-Pro.local>
2021-12-31 18:33:38 -0500

    submit collateral to lender

--- contracts/custodian.sol
+++ contracts/custodian.sol
@@ -1,14 +1,12 @@
 pragma solidity ^0.8.0;
 
-
 contract Custodian {
-   
+  
+
     struct Rules {
         uint maintenanceMargin;
         uint liquidationTime;
-        uint minimumPayment;
-        mapping(string =>bool) collateralApproved;
-        
+        uint gradePermitted;        
     }
 
     struct Collateral {
@@ -19,16 +17,65 @@ contract Custodian {
     }
 
     struct Trade {
+        uint id;
         address payable borrowerAdress;
-        address payable lenderAdress;
-        uint cashBorrowed;  
-        Collateral [] collateralPosted;
+        address lenderAdress;
+        uint cashBorrowed; 
+        Collateral [] collaterals;
+       // mapping(string =>Collateral) collateralsPosted;
         Rules rules;
+        bool approvedByCounterParty;
     }
-    
+  
+    uint tradeIndex = 0; 
+    uint indexColl=0;
+    mapping(uint => Trade) public trades;
+  
     constructor() {
 
     }
-    
+
+    modifier restricted(uint id) {
+        require(msg.sender == trades[id].borrowerAdress || msg.sender == trades[id].lenderAdress, "privilege acces");
+        _;
+    }
+    modifier tradeApproved(uint id) {
+        require(trades[id].approvedByCounterParty, "trade not approved by both parties");
+        _;
+    }
+
+    modifier collateralPermitted(uint id,Collateral memory collateral) {
+        require(trades[id].rules.gradePermitted==collateral.grade, "collateral is not permitted");
+        _;
+    }
+
+    function initiateContract( address payable borrowerAdress,uint cashBorrowed,uint maintenanceMargin,uint  liquidationTime,uint gradePermited ) public
+    {
+        uint id =random();
+       
+        Trade storage trade = trades[id];
+        trade.id=id;
+        trade.lenderAdress=msg.sender;
+        trade.borrowerAdress=borrowerAdress;
+        trade.cashBorrowed=cashBorrowed;
+        trade.rules.maintenanceMargin=maintenanceMargin;
+        trade.rules.liquidationTime=liquidationTime;
+        trade.rules.gradePermitted=gradePermited;
+        tradeIndex++;
+    }
+
+    function submitCollateral(Collateral memory collateral,uint id ) public restricted(id) tradeApproved(id) collateralPermitted(id,collateral) {
+        trades[id].collaterals[indexColl]=collateral;
+        indexColl++;
+    }
+
+
+    function approveContractByBorrower(bool approve,uint id ) public restricted(id){
+        trades[id].approvedByCounterParty=approve;
+    }
+
+      function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
+    }
 
 }
